710b9f428481548771bda3fac1644390376dd8dc
MLS <70773315+mlsfront@users.noreply.github.com>
2023-08-17 11:40:52 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,18 +1,22 @@
 function tabuada() {
-    let num = document.querySelector(`input#numText`);
-    let tab = document.querySelector(`select#seltab`);
+    let num = document.querySelector(`input#numText`)
+    let tab = document.querySelector(`select#seltab`)
     if (num.value.length == 0) {
-        window.alert(`Por favor, digite um número!`);
+        window.alert(`Por favor, digite um número!`)
     } else {
-        let n = Number(num.value);
+        let n = Number(num.value)
         let c = 1;
         tab.innerHTML = ``; // Limpa para fazer outra tabuada
         while (c <= 10) {
-            let item = document.createElement(`option`);    // cria um option no html
+            let item = document.createElement(`option`)    // cria um option no html
             item.text = `${n} x ${c} = ${n*c}`;         // mostra dentro do select
             item.value = `tab${c}`; // Para ser usada com outra lin, tipo PHP
-            tab.appendChild(item);
+            tab.appendChild(item)
             c++;
-        };
-    };  
-};
+        }
+    }  
+}
+
+function recarregar(){
+    window.location.reload(true);
+}
